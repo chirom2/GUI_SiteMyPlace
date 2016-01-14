c832fc007557a11dc6cef1e1d707d4a130be3781
chirom2_bucket <romain25.chiquet@gmail.com>
2016-01-14 20:30:41 +0100

    Ajout de vue et légers changement suite à la soutenance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$( document ).ready(function() {
+
+
+$("#quitter").click(function(){
+  $( "#dialog-confirm" ).dialog({
+        resizable: false,
+        height:140,
+        modal: true,
+        buttons: {
+          "Quitter la place": function() {
+            $( this ).dialog( "close" );
+          },
+          Annuler: function() {
+            $( this ).dialog( "close" );
+          }
+        }
+      });
+});
+
+
+
+
+    });
